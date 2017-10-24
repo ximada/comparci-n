a37fa6be546546ba415072dbc4a521f2c3098bcd
ximada <witch_ada@hotmail.com>
2017-10-24 09:36:45 -0500

    Actualizando el ejerccio

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 
-alert("hola chicas");
+var = function(arreglo1,arreglo2) {
+  if(arreglo1.length === arreglo2.length){
+retur false;
+  } else {
+    for(var i = 0; i < arreglo1.length; i +=)
+ {
+   if (arreglo1[i] !== arreglo2[i]){
+     document.write("todas la condiciones se cumplen");
+     retur false;
+
+   }
+ }  }
+
+}
+var array1 =[1,2,3,4,5,6,7,8,9,10];
+var array2 =[1,2,3,4,5,6,7,8,9,10];
+comparar(array1,array2);
